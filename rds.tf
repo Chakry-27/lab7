fb0c03abd3466b3df469110085ff82556d7af686
Chakry-27 <158128935+Chakry-27@users.noreply.github.com>
2024-07-06 12:44:36 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,15 +1,15 @@
 
 resource "aws_instance" "instance1" {
-  ami                         = "ami-0195204d5dce06d99"
+  ami                         = "ami-066f98455b59ca1ee"
   instance_type               = "t2.micro"
   count                       = 1
-  key_name                    = "kohli"
-  vpc_security_group_ids     = ["sg-0640ac226b79673e7"]
-  subnet_id                   = "subnet-001dafb658ac7d5b2"
+  key_name                    = "local"
+  vpc_security_group_ids     = ["sg-06751d2bc69d6b98b"]
+  subnet_id                   = "subnet-09b0644f6da29e605"
   associate_public_ip_address = true
   user_data                   = "${file("data.sh")}"
 
   tags = {
-    Name = "demoinstance"
+    Name = "chakry"
   }
 }
